cc97d9c5d7f6b9a0449a97edd664fc4ba2550862
unknown <ishdeshpa@gmail.com>
2021-03-31 11:53:01 -0500

    added compressor

--- Java/src/main/java/frc/robot/Robot.java
+++ Java/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -16,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
+  private Compressor comp;
   private RobotContainer m_robotContainer;
 
   /**
@@ -28,6 +29,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    comp = new Compressor();
+    comp.setClosedLoopControl(true);
   }
 
   /**
